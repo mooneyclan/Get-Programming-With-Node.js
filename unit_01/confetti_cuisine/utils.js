72c6c18dc5ffa85a5fa68045eae8f6f30de0dc23
William M <wmooney1984@gmail.com>
2020-01-21 05:51:22 -0500

    confetti_cuisine commpleted

--- unit_01/confetti_cuisine/utils.js
+++ unit_01/confetti_cuisine/utils.js
@@ -1,1 +1,15 @@
-"use strict";
+'use strict';
+const fs = require('fs'),
+  httpStatus = require('http-status-codes'),
+  contentTypes = require('./contentTypes');
+module.exports = {
+  getFile: (file, res) => {
+    fs.readFile(`./${file}`, (error, data) => {
+      if (error) {
+        res.writeHead(httpStatus.INTERNAL_SERVER_ERROR, contentTypes.html);
+        res.end('There was an error serving content!');
+      }
+      res.end(data);
+    });
+  }
+};
